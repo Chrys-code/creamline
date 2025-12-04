52a95f48f865addcab10cb9b00adcc035a631351
Krisztian Nagy <krisztian@MacBookAir.home>
2025-12-04 16:07:03 +0100

    more pdf export updates

--- src/services/apps/pdf_exports/use_cases/generate_milk_time_series_pdf.py
+++ src/services/apps/pdf_exports/use_cases/generate_milk_time_series_pdf.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 
 from weasyprint import HTML
 from apps.analytics.interfaces import MilkTimeSeriesReader
+from apps.milk.models import Milk
 
 
 class GenerateMilkTimeSeriesPdf:
@@ -12,12 +13,35 @@ class GenerateMilkTimeSeriesPdf:
     def execute(self, params: dict) -> HttpResponse:
         # 1. Fetch analytics data
         reader = MilkTimeSeriesReader()
-        data = reader.get(params)
+        summary_data = reader.get(params)
+
+        qs = Milk.objects.filter(
+            created_at__date__gte=params["start_date"],
+            created_at__date__lte=params["end_date"],
+        ).select_related("storage")
+
+        if params["producer_uuid"] and params["producer_uuid"].lower() != "all":
+            qs = qs.filter(producer__uuid=params["producer_uuid"])
+
+        instances = list(
+            qs.values(
+                "created_at",
+                "producer__name",
+                "volume_liters",
+                "storage__name",
+            ).order_by("created_at")
+        )
 
         # 2. Render HTML template
         html_string = render_to_string(
             "../templates/milk_time_series.html",
-            {"data": data, "params": params},
+            {
+                "data": {
+                    "summary": summary_data,
+                    "instances": instances,
+                },
+                "params": params,
+            },
         )
 
         # 3. Generate PDF in memory
