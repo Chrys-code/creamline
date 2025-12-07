a7925977f2356155af9bdeca98dc72503047302e
Krisztian Nagy <krisztian@Krisztians-MacBook-Air.local>
2025-12-07 01:00:15 +0100

    fix post migration signal to add groups and permissions

--- src/services/apps/users/features/user_groups/apps.py
+++ src/services/apps/users/features/user_groups/apps.py
@@ -1,10 +1,10 @@
 from django.apps import AppConfig
-
+from django.db.models.signals import post_migrate
 
 class UserGroupsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.users.features.user_groups"
 
     def ready(self):
-        # Register signals
-        import apps.users.features.user_groups.signals  # pylint: disable=unused-import, import-outside-toplevel
+        from apps.users.features.user_groups.signals import create_groups_and_assign_permissions  # pylint: disable=import-outside-toplevel
+        post_migrate.connect(create_groups_and_assign_permissions, sender=self)
--- src/services/apps/users/features/user_groups/signals.py
+++ src/services/apps/users/features/user_groups/signals.py
@@ -1,10 +1,12 @@
-from django.db.models.signals import post_migrate
 from django.contrib.auth.models import Group, Permission
 from apps.users.features.user_groups.constants import GROUPS_AND_PERMISSIONS
 from apps.users.features.user_groups.models import GroupMetadata
 
 
 def create_groups_and_assign_permissions(sender, **kwargs):
+    if sender.label != "auth":
+        return
+
     for (code_name, name), permission_codenames in GROUPS_AND_PERMISSIONS.items():
         group, _ = Group.objects.get_or_create(name=name)
         GroupMetadata.objects.get_or_create(
@@ -13,6 +15,3 @@ def create_groups_and_assign_permissions(sender, **kwargs):
 
         perms = Permission.objects.filter(codename__in=permission_codenames)
         group.permissions.set(perms)
-
-
-post_migrate.connect(create_groups_and_assign_permissions)
